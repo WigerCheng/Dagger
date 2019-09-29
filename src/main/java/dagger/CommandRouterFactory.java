9ac68375f73ab66b08026b095e567006ce061d3b
wiger <wiger980105@outlook.com>
2019-09-29 12:09:07 +0800

    https://dagger.dev/tutorial/05-abstraction-for-output

--- src/main/java/dagger/CommandRouterFactory.java
+++ src/main/java/dagger/CommandRouterFactory.java
@@ -1,7 +1,7 @@
 package dagger;
 
 /**
- * 为了告诉Dagger在中寻找该 @Binds方法 HelloWorldModule，我们将其添加到 @Component注释中。
+ * 为了告诉Dagger在中寻找该 @Binds方法 HelloWorldModule, SystemOutModule，我们将其添加到 @Component注释中。
  */
 @Component(modules = {HelloWorldModule.class, SystemOutModule.class})
 public interface CommandRouterFactory {
