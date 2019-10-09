4d27b1ac18b22e51ce9580a5965bf0b4a3597ba9
wigercheng <wigercheng@gmail.com>
2019-10-09 15:55:03 +0800

    https://dagger.dev/tutorial/11-withdraw-command

--- src/main/java/dagger/CommandProcessorFactory.java
+++ src/main/java/dagger/CommandProcessorFactory.java
@@ -3,7 +3,7 @@ package dagger;
 import javax.inject.Singleton;
 
 @Singleton
-@Component(modules = {SystemOutModule.class, CommandsModule.class, UserCommandsRouter.InstallationModule.class, AmountsModule.class})
+@Component(modules = {SystemOutModule.class, CommandsModule.class, UserCommandsRouter.InstallationModule.class})
 interface CommandProcessorFactory {
     CommandProcessor processor();
 }
